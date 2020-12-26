1a84e95ab17b4203c8248a4d6aa428a3751ab809
Ryan Au <auryan898@gmail.com>
2020-12-26 03:21:50 -0500

    Added status to client gamepad

--- browser-gamepads-broker/broker_server_ws.py
+++ browser-gamepads-broker/broker_server_ws.py
@@ -53,6 +53,6 @@ def hello():
 if __name__=='__main__':
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0',5000), app, handler_class=WebSocketHandler)
     print('Serving on port 5000')
     server.serve_forever()
